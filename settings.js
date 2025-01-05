f7d9971f4cf3401ef2deed7872e2db7d38c40261
Kingkgv <kgevans.12@gmail.com>
2025-01-05 14:52:31 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "KGEVANS"
 
-global.banknumber = "8133729715"
+global.banknumber = "254791002497"
 
-global.bankowner = "FADARE"
+global.bankowner = "KGEV@NS"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
